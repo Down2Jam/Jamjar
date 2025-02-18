4caee2d0ff82faedae76040603a5f80ba91ff68f
Ategon <benjamin@barbeau.net>
2025-02-18 14:30:31 -0500

    Fix page wrapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Sidebar from "@/components/sidebar";
 
 export default async function Home() {
   return (
-    <div className="flex justify-between flex-wrap">
+    <div className="flex justify-between flex-col md:flex-row">
       <div className="w-full md:w-2/3">
         <JamHeader />
         <Suspense fallback={<div>Loading...</div>}>
