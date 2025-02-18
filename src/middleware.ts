1c6871a921a79abc71301d62f2080d706737df74
Ategon <benjamin@barbeau.net>
2025-02-18 01:40:58 -0500

    Swap middleware to be a server side call

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getSelf } from "./requests/user";
+import { BASE_URL } from "./requests/config";
 
 export async function middleware(request: NextRequest) {
   try {
-    const userReponse = await getSelf();
-    if (userReponse.ok) return NextResponse.next();
+    const userCookie = request.cookies.get("user")?.value;
+    const tokenCookie = request.cookies.get("token")?.value;
+    const refreshCookie = request.cookies.get("refreshToken")?.value;
+
+    const userResponse = await fetch(
+      `${BASE_URL}/self?username=${userCookie}`,
+      {
+        headers: {
+          authorization: `Bearer ${tokenCookie}`,
+          refresh: refreshCookie || "",
+        },
+      }
+    );
+
+    if (userResponse.ok) return NextResponse.next();
   } catch (error) {
     console.error(error);
   }
