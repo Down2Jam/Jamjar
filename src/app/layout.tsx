4f74687fc75589888015813abed38680dcf23b11
Ategon <benjamin@barbeau.net>
2025-02-18 04:49:01 -0500

    Add patterned background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default async function RootLayout({
           <ThemeProvider attribute="class">
             <div className="">
               <div className="bg-[#fff] dark:bg-[#181818] min-h-screen flex flex-col ease-in-out transition-color duration-500">
+                <div className="fixed top-0 left-0 bg-opacity-5 bg-[repeating-linear-gradient(135deg,#075e94_0px,#075e94_40px,#4a3279_40px,#4a3279_80px)] w-screen h-screen opacity-10 dark:opacity-5" />
                 <Navbar />
                 <Spacer y={5} />
                 <div className="max-w-6xl xl:max-w-7xl 2xl:max-w-8xl mx-auto flex-grow w-full">
@@ -41,3 +42,7 @@ export default async function RootLayout({
     </html>
   );
 }
+
+/*
+<div className="fixed top-0 left-0 bg-opacity-5 bg-gradient-to-br from-[#075e94] to-[#4a3279] via-transparent w-screen h-screen opacity-10" />
+*/
