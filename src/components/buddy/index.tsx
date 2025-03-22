0020a92373311c81d363c3f13dbea72404b4c882
Ategon <benjamin@barbeau.net>
2025-03-22 13:56:01 -0400

    Add quotes

--- src/components/buddy/index.tsx
+++ src/components/buddy/index.tsx
@@ -7,6 +7,17 @@ import { useEffect, useState } from "react";
 export default function Buddy() {
   const [isAnimating, setIsAnimating] = useState(false);
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [tooltipText, setTooltipText] = useState("Choo Choo");
+
+  const tooltips = [
+    "Choo Choo",
+    "I think, therefore I train",
+    "To train or not to train",
+    "I have a dream... of a train",
+    "Theres a train behind you",
+    "Houston, we have a train",
+    "When life gives you lemons, make a lemon train",
+  ];
 
   useEffect(() => {
     setAudio(new Audio("/sounds/train.mp3")); // Change to your sound file path
@@ -22,6 +33,13 @@ export default function Buddy() {
     setTimeout(() => setIsAnimating(false), 300); // Reset animation
   };
 
+  const handleHover = (isOpen: boolean) => {
+    if (!isOpen) return;
+
+    const randomIndex = Math.floor(Math.random() * tooltips.length);
+    setTooltipText(tooltips[randomIndex]);
+  };
+
   return (
     <div
       className={`fixed z-50 right-2 bottom-2 hover:scale-105 hover:cursor-pointer duration-200 transition-all ${
@@ -29,7 +47,7 @@ export default function Buddy() {
       }`}
       onClick={handleClick}
     >
-      <Tooltip content="Choo Choo" closeDelay={0}>
+      <Tooltip content={tooltipText} closeDelay={0} onOpenChange={handleHover}>
         <Image
           src="/images/Train.png"
           alt="Train buddy"
