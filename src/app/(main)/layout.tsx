d661a6bfde9b6168ce993c556dd8b7df0f82c002
Ategon <benjamin@barbeau.net>
2025-09-21 12:51:56 -0400

    Fix graph spacing

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,3 @@
-import Buddy from "@/components/buddy";
 import Navbar from "../../components/navbar";
 import PageBackground from "./PageBackground";
 
@@ -12,7 +11,6 @@ export default async function RootLayout({
   return (
     <PageBackground>
       <Navbar />
-      <Buddy />
       <div className="mt-4 max-w-6xl xl:max-w-7xl 2xl:max-w-8xl mx-auto flex-grow w-full px-2 sm:px-8 z-10">
         {children}
       </div>
--- src/components/results/index.tsx
+++ src/components/results/index.tsx
@@ -413,7 +413,7 @@ export default function Results() {
                         );
                       })}
                   </div>
-                  <div className="w-40 h-32 shrink-0">
+                  <div className="w-80 h-32 shrink-0">
                     <ResponsiveContainer width="100%" height="100%">
                       <RadarChart
                         cx="50%"
