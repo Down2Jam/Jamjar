656483a80adf2607b62cc1c20a617708cf994183
cgsdev0 <git@sarah.engineer>
2026-03-22 06:48:40 -0700

    proxy to prod api (#140)

--- src/requests/config.ts
+++ src/requests/config.ts
@@ -1,4 +1,6 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_MODE === "PROD"
     ? "https://d2jam.com/api/v1"
-    : "http://localhost:3005/api/v1";
+    : process.env.NEXT_PUBLIC_API === "prod"
+      ? "http://localhost:3000/api/v1"
+      : "http://localhost:3005/api/v1";
