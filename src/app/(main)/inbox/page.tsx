cc968015ceb0c9aa0b04ecd16e979ee147c2769a
Ategon <benjamin@barbeau.net>
2026-03-23 11:13:37 -0400

    Stretch notifications

--- src/app/(main)/inbox/page.tsx
+++ src/app/(main)/inbox/page.tsx
@@ -41,83 +41,85 @@ export default function InboxPage() {
         </Vstack>
       </Card>
 
-      {notifications.map((notification) => {
-        const handleMarkRead = async (id: number) => {
-          const res = await deleteNotification(id);
-          if (res.ok) await removeNotificationFromState(id);
-        };
+      <Vstack align="stretch">
+        {notifications.map((notification) => {
+          const handleMarkRead = async (id: number) => {
+            const res = await deleteNotification(id);
+            if (res.ok) await removeNotificationFromState(id);
+          };
 
-        switch (notification.type) {
-          case "TEAM_INVITE":
-            return (
-              <TeamInviteNotification
-                key={notification.id}
-                notification={notification}
-                onAccept={async (inviteId, notificationId) => {
-                  try {
-                    const ok = await handleInvite(inviteId, true);
-                    if (ok) await handleMarkRead(notificationId);
-                  } catch (e) {
-                    console.error(e);
-                  }
-                }}
-                onReject={async (inviteId, notificationId) => {
-                  try {
-                    const ok = await handleInvite(inviteId, false);
-                    if (ok) await handleMarkRead(notificationId);
-                  } catch (e) {
-                    console.error(e);
-                  }
-                }}
-              />
-            );
+          switch (notification.type) {
+            case "TEAM_INVITE":
+              return (
+                <TeamInviteNotification
+                  key={notification.id}
+                  notification={notification}
+                  onAccept={async (inviteId, notificationId) => {
+                    try {
+                      const ok = await handleInvite(inviteId, true);
+                      if (ok) await handleMarkRead(notificationId);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                  onReject={async (inviteId, notificationId) => {
+                    try {
+                      const ok = await handleInvite(inviteId, false);
+                      if (ok) await handleMarkRead(notificationId);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                />
+              );
 
-          case "TEAM_APPLICATION":
-            return (
-              <TeamApplicationNotification
-                key={notification.id}
-                notification={notification}
-                onAccept={async (applicationId, notificationId) => {
-                  try {
-                    const ok = await handleApplication(applicationId, true);
-                    if (ok) await handleMarkRead(notificationId);
-                  } catch (e) {
-                    console.error(e);
-                  }
-                }}
-                onReject={async (applicationId, notificationId) => {
-                  try {
-                    const ok = await handleApplication(applicationId, false);
-                    if (ok) await handleMarkRead(notificationId);
-                  } catch (e) {
-                    console.error(e);
-                  }
-                }}
-              />
-            );
+            case "TEAM_APPLICATION":
+              return (
+                <TeamApplicationNotification
+                  key={notification.id}
+                  notification={notification}
+                  onAccept={async (applicationId, notificationId) => {
+                    try {
+                      const ok = await handleApplication(applicationId, true);
+                      if (ok) await handleMarkRead(notificationId);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                  onReject={async (applicationId, notificationId) => {
+                    try {
+                      const ok = await handleApplication(applicationId, false);
+                      if (ok) await handleMarkRead(notificationId);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                />
+              );
 
-          case "GAME_COMMENT":
-          case "TRACK_COMMENT":
-          case "POST_COMMENT":
-          case "COMMENT_REPLY":
-            return (
-              <CommentNotification
-                key={notification.id}
-                notification={notification}
-                onMarkRead={handleMarkRead}
-              />
-            );
+            case "GAME_COMMENT":
+            case "TRACK_COMMENT":
+            case "POST_COMMENT":
+            case "COMMENT_REPLY":
+              return (
+                <CommentNotification
+                  key={notification.id}
+                  notification={notification}
+                  onMarkRead={handleMarkRead}
+                />
+              );
 
-          default:
-            return (
-              <GeneralNotification
-                key={notification.id}
-                notification={notification}
-                onMarkRead={handleMarkRead}
-              />
-            );
-        }
-      })}
+            default:
+              return (
+                <GeneralNotification
+                  key={notification.id}
+                  notification={notification}
+                  onMarkRead={handleMarkRead}
+                />
+              );
+          }
+        })}
+      </Vstack>
     </Vstack>
   );
 }
